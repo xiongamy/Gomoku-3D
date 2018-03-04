fb61ff28d135cb05fdeed29858f34d7a6c943006
charleslien <charleslien97@gmail.com>
2018-03-03 21:55:05 -0800

    changed gitignore

---
+++ Assets/Scripts/Arcball.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+
+public class Arcball : MonoBehaviour
+{
+
+    public float speed;
+    
+    void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X");
+        float rotY = Input.GetAxis("Mouse Y");
+        // transform.Rotate(Vector3.up, -rotX);
+        // transform.Rotate(Vector3.right, rotY);
+        transform.Rotate(-rotY * speed, rotX * speed, 0);
+    }
+}
